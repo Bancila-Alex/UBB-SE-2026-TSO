2128df061ca99610925a23b7a735927dd9bde4c0
paulraresbahamat <paul.bahamat@stud.ubbcluj.ro>
2026-04-01 15:56:27 +0300

    Finished Task 34

---
+++ ChatModule/ChatModule/src/services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using BCrypt.Net;
+
+namespace ChatModule.Services
+{
+    public class AuthService
+    {
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        private bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
