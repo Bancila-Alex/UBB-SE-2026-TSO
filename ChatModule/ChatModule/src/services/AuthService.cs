18b71538587f2261c5cab102ddf4cd4bc3631a2b
paulraresbahamat <paul.bahamat@stud.ubbcluj.ro>
2026-04-01 16:22:44 +0300

    Finished Task-35

--- ChatModule/ChatModule/src/services/AuthService.cs
+++ ChatModule/ChatModule/src/services/AuthService.cs
@@ -5,11 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using BCrypt.Net;
+using ChatModule.Repositories;
+using ChatModule.Models;
 
 namespace ChatModule.Services
 {
     public class AuthService
     {
+        private readonly UserRepository _userRepository;
+
+        public AuthService(UserRepository userRepository) {  _userRepository = userRepository; }
         private string HashPassword(string password)
         {
             return BCrypt.Net.BCrypt.HashPassword(password);
@@ -18,6 +23,20 @@ namespace ChatModule.Services
         private bool VerifyPassword(string password, string hash)
         {
             return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+
+        public async Task<User?> LoginAsync(string username, string password)
+        {
+            var user = await _userRepository.GetByUsernameAsync(username);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (!VerifyPassword(password, user.PasswordHash))
+                return null;
+
+            return user;
         }
     }
 }
