34a36b9561c1bade2a35613b62688a61a3b8a99d
Bancila-Alex <146870068+Bancinatorul@users.noreply.github.com>
2026-04-02 20:26:23 +0300

    Fried List View Model

--- ChatModule/ChatModule/src/view_models/MainViewModel.cs
+++ ChatModule/ChatModule/src/view_models/MainViewModel.cs
@@ -9,6 +9,7 @@ namespace ChatModule.ViewModels
     {
         private readonly ConversationListService _conversationListService;
         private readonly FriendRequestService _friendRequestService;
+        private readonly FriendListService? _friendListService;
         private readonly BlockService _blockService;
         private readonly DirectMessageService _directMessageService;
         private readonly ProfileService _profileService;
@@ -35,6 +36,7 @@ namespace ChatModule.ViewModels
         }
 
         public event Action? NavigateToLoginRequested;
+        public event Action<Guid>? NavigateToChatRequested;
 
         public RelayCommand GoToConversationsCommand { get; }
         public RelayCommand GoToFriendsCommand { get; }
@@ -47,9 +49,27 @@ namespace ChatModule.ViewModels
             BlockService blockService,
             ProfileService profileService,
             DirectMessageService directMessageService)
+            : this(
+                conversationListService,
+                friendRequestService,
+                friendListService: null,
+                blockService,
+                profileService,
+                directMessageService)
+        {
+        }
+
+        public MainViewModel(
+            ConversationListService conversationListService,
+            FriendRequestService friendRequestService,
+            FriendListService? friendListService,
+            BlockService blockService,
+            ProfileService profileService,
+            DirectMessageService directMessageService)
         {
             _conversationListService = conversationListService ?? throw new ArgumentNullException(nameof(conversationListService));
             _friendRequestService = friendRequestService ?? throw new ArgumentNullException(nameof(friendRequestService));
+            _friendListService = friendListService;
             _blockService = blockService ?? throw new ArgumentNullException(nameof(blockService));
             _profileService = profileService ?? throw new ArgumentNullException(nameof(profileService));
             _directMessageService = directMessageService ?? throw new ArgumentNullException(nameof(directMessageService));
@@ -76,14 +96,37 @@ namespace ChatModule.ViewModels
 
         private Task GoToFriendsAsync()
         {
+            if (_friendListService != null)
+            {
+                var friendListViewModel = new FriendListViewModel(_friendListService, _directMessageService, CurrentUserId);
+                friendListViewModel.NavigateToProfileRequested += OnNavigateToProfileFromFriends;
+                friendListViewModel.NavigateToChatRequested += OnNavigateToChatFromFriends;
+                CurrentPage = friendListViewModel;
+                return Task.CompletedTask;
+            }
+
             CurrentPage = new FriendRequestsViewModel(_friendRequestService, CurrentUserId);
             return Task.CompletedTask;
         }
 
         private Task GoToProfileAsync()
+            => ShowProfileAsync(CurrentUserId);
+
+        private void OnNavigateToProfileFromFriends(Guid userId)
         {
-            CurrentPage = new ProfileViewModel(_friendRequestService, _blockService, _directMessageService, _profileService, CurrentUserId);
-            return Task.CompletedTask;
+            _ = ShowProfileAsync(userId);
+        }
+
+        private void OnNavigateToChatFromFriends(Guid conversationId)
+        {
+            NavigateToChatRequested?.Invoke(conversationId);
+        }
+
+        private async Task ShowProfileAsync(Guid targetUserId)
+        {
+            var profileViewModel = new ProfileViewModel(_friendRequestService, _blockService, _directMessageService, _profileService, CurrentUserId);
+            await profileViewModel.LoadAsync(targetUserId);
+            CurrentPage = profileViewModel;
         }
 
         private Task LogoutAsync()
